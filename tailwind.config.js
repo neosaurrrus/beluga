342e350e5306ec17622878961f46bad98d33986f
Lukie Kang <lukiek@gmail.com>
2022-08-21 00:56:09 +0100

    style: add basic styles as a placeholder to keep my sanity white working on content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      'display': ['Fjalla One', 'sans-serif'],
+      'body': ['Nunito', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
