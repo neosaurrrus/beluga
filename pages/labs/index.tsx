ef42818073e2e42e12fd64edd04becc1028339c1
Lukie Kang <lukiek@gmail.com>
2022-08-21 23:04:30 +0100

    docs: rework copy

--- pages/labs/index.tsx
+++ pages/labs/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const LabsPage: NextPage = () => (
   <section>
     <h1>Secret Labs</h1>
-    <p>Sometimes I just want to run some experiments in a safe space. This is my safe space, hopefully hidden away from the world. Unless they look at the repo</p>
+    <p>Sometimes I just want to run some experiments in simplified scenarios. This is my safe space, hopefully hidden away from the world. Unless they look at the repo</p>
     <ul>
       <li>
         <Link href='/labs/next-data-fetching'>
